ccde068396fc082d46745a6a8e07594836fbfd6a
abhay1158 <abhayp.singh200029@gmail.com>
2024-05-15 23:06:23 +0530

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import NavBar from "./NavBar";
 import SidePanel from "./SidePanel";
 import Loans from "./Loans";
-import "./App.css";
+
 
 function App() {
   return (
